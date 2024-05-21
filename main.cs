ee673a803bd7abf74e8a8173c69f3cea37e1fe8a
jkelly31 <142122577+jkelly31@users.noreply.github.com>
2024-05-21 03:53:27 +0000

    PersonalInfo

--- main.cs
+++ main.cs
@@ -1,7 +1,31 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+  public static void Main() {
+    Console.WriteLine ("Lucinda Potter");
+    Console.WriteLine ("6/24/1992");
+    Console.WriteLine ("work 000-000-0101");
+    Console.WriteLine ("cell 000-000-0189");
+    Console.WriteLine ();
+
+    Console.WriteLine ("John Smith");
+    Console.WriteLine ("8/15/1978");
+    Console.WriteLine ("work 000-111-0101");
+    Console.WriteLine ("cell 000-111-0189");
+    Console.WriteLine ();
+
+    Console.WriteLine ("Chris Johnson");
+    Console.WriteLine ("12/01/1987");
+    Console.WriteLine ("work 000-222-0101");
+    Console.WriteLine ("cell 000-222-0189");
+    Console.WriteLine ();
+
+    Console.WriteLine ("Robert Hall");
+    Console.WriteLine ("2/27/1949");
+    Console.WriteLine ("work 000-444-0101");
+    Console.WriteLine ("cell 000-444-0189");
+    Console.WriteLine ();
+    
   }
-}
+
+  }
