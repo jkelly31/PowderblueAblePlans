a6f7181ed0e644966e566404a0bf64394ebf2d32
jkelly31 <142122577+jkelly31@users.noreply.github.com>
2024-06-01 02:44:01 +0000

    InchesToCentimeters

--- main.cs
+++ main.cs
@@ -1,14 +1,27 @@
 using System;
 
 class Program {
-  public static void Main() {
-    Console.WriteLine ("Oh, well imagine /");
-    Console.WriteLine ("As I'm pacing the pews in a church corridor /");
-    Console.WriteLine ("And I can't help but to hear /");
-    Console.WriteLine ("No, I can't help but to hear an exchanging of words...");
-    Console.WriteLine ();
 
+      static void Main()
+      {
+          const double CentimetersPerInch = 2.54;
+
+          double inches = 5; // Required Input: 5 inches
+
+          double centimeters = inches * CentimetersPerInch;
+
+          Console.WriteLine($"{centimeters} centimeters");
+      }
+  }
+
+  
     
-  }
+    //Console.WriteLine("({0} + {1}) * {2} = {3}", x, y, z, total);
+    //Console.WriteLine("The value of y is {0}", y);
+    
+    //string declareString = "Declared String";
+    //Console.Write(declareString);
 
-  }
+    //var fullName = ConcatName("Julia", "Kelly");
+    //Console.WriteLine("Hello world! {0} is here and {1} likes cats", fullName, fullName);
+
