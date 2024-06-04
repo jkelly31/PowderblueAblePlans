ef2d40e4a5fab757a1b93e4066a158433ba77bbb
jkelly31 <142122577+jkelly31@users.noreply.github.com>
2024-06-04 21:58:52 +0000

    Twitter

--- main.cs
+++ main.cs
@@ -19,13 +19,4 @@ class Program {
   }
 
   
-    
-    //Console.WriteLine("({0} + {1}) * {2} = {3}", x, y, z, total);
-    //Console.WriteLine("The value of y is {0}", y);
-    
-    //string declareString = "Declared String";
-    //Console.Write(declareString);
-
-    //var fullName = ConcatName("Julia", "Kelly");
-    //Console.WriteLine("Hello world! {0} is here and {1} likes cats", fullName, fullName);
 
