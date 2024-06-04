0db3634771b0956b5af8f92b8b44367ecd933e6e
jkelly31 <142122577+jkelly31@users.noreply.github.com>
2024-06-04 21:57:12 +0000

    Twitter

--- main.cs
+++ main.cs
@@ -2,22 +2,20 @@ using System;
 
 class Program {
 
-      static void Main()
+    static void Main(string[] args)
+  {
+      Console.WriteLine("Please enter your message:");
+      string userMessage = Console.ReadLine();
+
+      if (userMessage.Length <= 140)
       {
-        const double BaseRate = 200.0;
-        const double HourlyRate = 150.0;
-        const double MileRate = 2.0;
-
-        Console.WriteLine("Enter the number of hours for the job:");
-        double hours = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Enter the number of miles for the move:");
-        double miles = double.Parse(Console.ReadLine());
-
-        double totalFee = BaseRate + (hours * HourlyRate) + (miles * MileRate);
-
-        Console.WriteLine($"Total moving fee: ${totalFee:F2}");
+          Console.WriteLine("Your message is: Posted");
       }
+      else
+      {
+          Console.WriteLine("Your message is: Rejected");
+      }
+  }
   }
 
   
