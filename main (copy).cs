6ce66ee1ef02a900ca05f15103376d67c8b8e1b3
jkelly31 <142122577+jkelly31@users.noreply.github.com>
2024-07-27 19:41:00 +0000

    Exercise 1.2 V2

---
+++ main (copy).cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program {
+    public static void Main() {
+        // unit test 1
+        Console.WriteLine ("Oh, well imagine /");
+        Console.WriteLine ("As I'm pacing the pews in a church corridor /");
+        Console.WriteLine ("And I can't help but to hear /");
+        Console.WriteLine ("No, I can't help but to hear an exchanging of words...");
+        Console.WriteLine ();
+        // unit test 2
+        Console.WriteLine ("Oh-oh-oh-oh-oh,");
+        Console.WriteLine ("oh-oh-oh-oh,");
+        Console.WriteLine ("oh-oh-oh /");
+        Console.WriteLine ("Caught in a bad romance...");
+        Console.WriteLine ();
+        // unit test 3
+        Console.WriteLine ("Coming out of my cage");
+        Console.WriteLine ("And I've been doing just fine");
+        Console.WriteLine ("Gotta gotta be down");
+        Console.WriteLine ("Because I want it all");
+        Console.WriteLine ();
+        // unit test 4
+        Console.WriteLine ("Now, this is a story all about how");
+        Console.WriteLine ("My life got flipped turned upside down");
+        Console.WriteLine ("And I'd like to take a minute");
+        Console.WriteLine ("Just sit right there");
+        Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
+        Console.WriteLine ();
+    }
+}
